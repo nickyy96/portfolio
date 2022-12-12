dd7664f7ddaf2c3c0d60507bcb7083365a3da65a
nickyy96 <nick.yarnall9@gmail.com>
2022-12-12 16:47:24 -0500

    FEAT: updated title - finished with project for UI/UX

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,8 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <title>nicky's portfolio</title>
+        {/* <title>nicky's portfolio</title> */}
+        <title>lazy's portfolio</title>
         <Head>
           <script src="/theme.js"></script>
           <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;600&family=Urbanist:wght@100;400&display=swap" rel="stylesheet"></link>
